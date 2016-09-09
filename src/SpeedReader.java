72a57bf715e7d8e6b14c62d58d5147da849b88a6
Maddie Kirwin <kirwinma@cs.grinnell.edu>
2016-09-08 21:00:57 -0500

    Updated homework

--- src/SpeedReader.java
+++ src/SpeedReader.java
@@ -2,12 +2,8 @@ import java.awt.*;
 import java.io.IOException;
 
 public class SpeedReader {
-	
-	public void renderWord(Graphics g, String word, int width, int height) {
-	    g.drawString(word, width / 4, height / 4);
-	}
-	
-	public void main(String[] args) throws IOException, InterruptedException {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
 		if (args.length != 5) {
 			System.out.println("Usage: SpeedReader <filename> <width> <height> <font size> <wpm>");
 			return;
@@ -17,19 +13,22 @@ public class SpeedReader {
 		int height = Integer.parseInt(args[2]);
 		int fontSize = Integer.parseInt(args[3]);
 		int wpm = Integer.parseInt(args[4]);
-		
+
 		DrawingPanel panel = new DrawingPanel(width, height);
-	    Graphics g = panel.getGraphics();
-	    Font f = new Font("Courier", Font.BOLD, fontSize);
-	    g.setFont(f);
-	    
-	    WordGenerator text = new WordGenerator(filename);
-	    
-	    while (text.hasNext()) {
-	    	renderWord(g, text.next(), width, height);
-	    	Thread.sleep(60000/wpm);
-	    }
-		
-		
+		Graphics g = panel.getGraphics();
+		Font f = new Font("Courier", Font.BOLD, fontSize);
+		g.setFont(f);
+
+		WordGenerator text = new WordGenerator(filename);
+
+		while (text.hasNext()) {
+			g.setColor(Color.black);
+			g.drawString(text.next(), width / 2, height / 2);
+			Thread.sleep(60000 / wpm);
+			g.setColor(Color.white);
+			g.fillRect(0, 0, width, height);
+		}
+
+
 	}
 }
